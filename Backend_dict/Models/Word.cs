fc7aad239ca7a28607c7db572a932c899363b56a
Burak Özgen <132925731+i-burakozgen@users.noreply.github.com>
2024-09-01 20:06:58 +0300

    initial commit

---
+++ Backend_dict/Models/Word.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Backend_dict.Models;
+
+public class Word
+{
+    [Key]
+    public int Id { get; set; }
+    public int WordName { get; set; }
+    public ICollection<Variation> Variations { get; set; }
+    public ICollection<Meaning> Meanings { get; set; }
+    public ICollection<PersianTransliteration> PersianTransliterations { get; set; }
+}
