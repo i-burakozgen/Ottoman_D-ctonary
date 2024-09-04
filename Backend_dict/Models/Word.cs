6d30653c4783a7f0b43f27e515c3b1a8c23afbe9
Burak Özgen <132925731+i-burakozgen@users.noreply.github.com>
2024-09-04 16:18:23 +0300

    database is filled

--- Backend_dict/Models/Word.cs
+++ Backend_dict/Models/Word.cs
@@ -6,8 +6,11 @@ public class Word
 {
     [Key]
     public int Id { get; set; }
-    public int WordName { get; set; }
+    public string WordName { get; set; }
     public ICollection<Variation> Variations { get; set; }
     public ICollection<Meaning> Meanings { get; set; }
     public ICollection<PersianTransliteration> PersianTransliterations { get; set; }
 }
+
+
+
